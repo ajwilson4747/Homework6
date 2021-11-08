01968054c9eda9e2eb52fdb5c313614590a66516
ajwilson4747 <83085316+ajwilson4747@users.noreply.github.com>
2021-11-08 00:39:48 -0700

    Add files via upload
    This is the updated version of the Main_Count class

--- Main_Count.java
+++ Main_Count.java
@@ -22,54 +22,53 @@ public class Main_Count {
 		// create a scanner to read the file
 		Scanner myScanner = null;
 		// use try and catch to open file and read it
-		
+
 		try {
 			// instantiate Scanner object
 			myScanner = new Scanner(myFile);
-
-			/*
-			 * // instantiate the an arraylist ArrayList<String> list =
-			 * createArrayListMCS(myScanner);
-			 * 
-			 * // parse through the arraylist and place every line in a row for (int i = 0;
-			 * i <list.size(); i++) { myView.row[0] = list.get(i);
-			 * myView.model.addRow(myView.row); }
-			 */
-			 
-			
 			// count the words
-			countKeyWords(myScanner);
+			//countKeyWords(myScanner);
+			// instantiate the an arraylist ArrayList<String> list =
+			ArrayList<String> mylist = createArrayListMCS(myScanner);
+			//parse through the arraylist and place every line in a row ;
+			
+			  for (int i = 0; i < mylist.size(); i++) { myView.row[0] = mylist.get(i); 
+			  myView.model.addRow(myView.row); }
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.print(" This file can not be found ~ Error");
 		} finally {
-			
+
 			if (myScanner != null)
-			myScanner.close(); // close the scanner object
+				myScanner.close(); // close the scanner object
 		}
 	}
 
 	// create ArrayList
 	public static ArrayList<String> createArrayListMCS(Scanner myScanner) {
-		// TASK 1 : CREATE A STACK TO IDENTIFY MCS
 
-		Stack  myStack = new Stack();
+		// TASK 1 : CREATE A STACK TO IDENTIFY MCS
+		Stack<String> myStack = new Stack<String>();
 
 		// TASK 2: CREATE VARIABLES TO HOLD THE CURRENT AND PREVIOUS INSTRUCTIONS
 		String currentCodeLine = "";
 		String previousLine = "";
-
+		int totalSum = 0;
 		// TASK 3: CREATE AN ARRAYLIST TO STORE ALL THE MCSs
 		ArrayList<String> list = new ArrayList<String>();
 
-		// TASK 4: SCAN THE CODE LINE BY LINE. PUSH IF { AND POP IF }
-
+			
+		
+			// TASK 4: SCAN THE CODE LINE BY LINE. PUSH IF { AND POP IF }
 		while (myScanner.hasNextLine()) {
+
 			currentCodeLine = myScanner.nextLine();
 			currentCodeLine = currentCodeLine.trim();
 
 			// Is it a codeline
 			if (currentCodeLine.length() > 0) {
+				totalSum += 1;
 				// just a regular code line
 				if (!currentCodeLine.equals("{") && !currentCodeLine.equals("}"))
 					previousLine = currentCodeLine;
@@ -80,12 +79,16 @@ public class Main_Count {
 
 				// if } is present
 				else
-					list.add(0, myStack.pop());
+					list.add(0, (String) myStack.pop());
 			}
 		}
-
+		
+		
+		myView.row[2] = totalSum;
+		myView.model.addRow(myView.row);
 		// return the arraylist
 		return list;
+
 	}
 
 	// This will count everything FOR NOW
@@ -109,12 +112,6 @@ public class Main_Count {
 				}
 			}
 		}
-
-		// print out values from the map
-		/*
-		 * for (String s: keywordTreeMap.keySet()) { myView.columns[1] =
-		 * keywordTreeMap.get(s); }
-		 */
 		keywordTreeMap.forEach((key, value) -> System.out.println(key + "\t" + value));
 	}
 
